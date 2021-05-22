dab7ac409ee140c09f76b35aedc01749921ff262
Trim21 <i@trim21.me>
2021-05-22 17:07:38 +0800

    fix

--- src/main.ts
+++ src/main.ts
@@ -16,12 +16,13 @@ async function run(): Promise<void> {
   if (!installedVersion) {
     installedVersion = await getLatestPoetryVersion();
   }
-  const args = [path.join(tmpDir, "install-poetry.py"), "--yes", "--version", installedVersion];
+  const installerPath = path.join(tmpDir, "install-poetry.py");
+  const args = [installerPath, "--yes", "--version", installedVersion];
   const poetryHome = path.join(os.homedir(), ".poetry");
 
   if (!(await cache.restore(pythonVersion, installedVersion))) {
     const installer = "https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py";
-    await exec(`curl -sSL ${installer} -o ${tmpDir}/install-poetry.py`);
+    await exec("curl", ["-sSL", installer, "-o", installerPath]);
     await exec("python", args,
       {
         env: {
