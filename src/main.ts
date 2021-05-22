e7cd6a4e4d7b8bc20be4e96e05530f7415848258
Trim21 <i@trim21.me>
2021-05-22 17:40:17 +0800

    fix

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,15 @@ import * as path from "path";
 import * as core from "@actions/core";
 import { exec } from "@actions/exec";
 
-import { getLatestPoetryVersion, getPythonVersion, getTmpDir } from "./utils";
-import * as cache from "./cache";
+import { getLatestPoetryVersion, getTmpDir } from "./utils";
+
+// import * as cache from "./cache";
 
 async function run(): Promise<void> {
   let installedVersion = core.getInput("version");
   // const preview = core.getInput('preview')
   const tmpDir = getTmpDir();
-  const pythonVersion = await getPythonVersion();
+  // const pythonVersion = await getPythonVersion();
   //
   if (!installedVersion) {
     installedVersion = await getLatestPoetryVersion();
@@ -19,17 +20,17 @@ async function run(): Promise<void> {
   const installerPath = path.join(tmpDir, "install-poetry.py");
   const poetryHome = path.join(os.homedir(), ".poetry");
 
-  if (!(await cache.restore(pythonVersion, installedVersion))) {
-    const installer = "https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py";
-    await exec("curl", ["-sSL", installer, "-o", installerPath]);
-    await exec("python", [installerPath, "--yes", "--version", installedVersion],
-      {
-        env: {
-          POETRY_HOME: poetryHome,
-        },
-      });
-    await cache.setup(pythonVersion, installedVersion);
-  }
+  // if (!(await cache.restore(pythonVersion, installedVersion))) {
+  const installer = "https://raw.githubusercontent.com/python-poetry/poetry/master/install-poetry.py";
+  await exec("curl", ["-sSL", installer, "-o", installerPath]);
+  await exec("python", [installerPath, "--yes", "--version", installedVersion],
+    {
+      env: {
+        POETRY_HOME: poetryHome,
+      },
+    });
+  // await cache.setup(pythonVersion, installedVersion);
+  // }
   core.info(path.join(poetryHome, "bin"));
 }
 
