ab40dfe420bdb3fe731b024444289c23aeaf6e20
Trim21 <trim21.me@gmail.com>
2023-04-28 11:51:23 +0800

    docs: add more pep440 range example

--- README.md
+++ README.md
@@ -16,3 +16,4 @@ for example
 - `>=1.2,<2.0`
 - `~=1.2` (version 1.2 or later, but not version 2.0 or later)
 - `== 3.1.*` (`==` is required, `3.1.*` and `3.1.x` won't work)
+- `~=1.1.0, != 1.1.3`: version 1.1.0 or later, but not version 1.1.3 and not version 1.2.0 or later.
--- __tests__/util.test.ts
+++ __tests__/util.test.ts
@@ -8,4 +8,5 @@ test("select latest version", () => {
   expect(getLatestMatchedVersion(["1.0.0", "1.2.3"], ">1.2.0")).toBe("1.2.3");
   expect(getLatestMatchedVersion(["1.0.0", "1.2.3"], "==1.2.*")).toBe("1.2.3");
   expect(getLatestMatchedVersion(["1.0.0", "1.2.3"], ">=1.3,<2.0")).toBe(null);
+  expect(getLatestMatchedVersion(["1.1.3", "1.1.2", "1.2.3"], "~=1.1.0, != 1.1.3")).toBe("1.1.2");
 });
